f45425a5f8ede2b54001da2034e645fa11882736
Mahen Abeywickrama <mahenabeywickrema0@gmail.com>
2025-11-19 22:29:02 +0530

    user model

---
+++ src/models/User.ts
@@ -0,0 +1,26 @@
+import mongoose, { Document, Schema } from "mongoose"
+
+export enum Role {
+  ADMIN = "ADMIN",
+  AUTHOR = "AUTHOR",
+  USER = "USER"
+}
+
+export interface IUser extends Document {
+  _id: mongoose.Types.ObjectId
+  firstname: string
+  lastname: string
+  email: string
+  password: string
+  roles: Role
+}
+
+const userSchema = new Schema<IUser>({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, required: true },
+  roles: { type: String, enum: Object.values(Role), default: Role.USER }
+})
+
+export const User = mongoose.model<IUser>("User", userSchema)
