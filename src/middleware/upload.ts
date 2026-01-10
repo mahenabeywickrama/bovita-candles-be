4eb6aca3b576f7e5358ce244a66c18abb61a8f1a
Mahen Abeywickrama <mahenabeywickrema0@gmail.com>
2026-01-10 11:01:57 +0530

    project updated

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -5,7 +5,7 @@ const storage = multer.memoryStorage()
 export const upload = multer({
   storage,
   limits: {
-    fileSize: 5 * 1024 * 1024,
+    fileSize: 20 * 1024 * 1024,
     files: 5,
   }
 })
